474fe372be22151ccf9385c86b1e92e41672aa9a
Kobe Kamin <97008293+BaconThePoro@users.noreply.github.com>
2023-07-13 19:25:22 -0700

    cringe

--- DigitalPrototype/Assets/Scripts/Multiplayer/MGameController.cs
+++ DigitalPrototype/Assets/Scripts/Multiplayer/MGameController.cs
@@ -3623,7 +3623,9 @@ public class MGameController : NetworkBehaviour
     
     public IEnumerator LerpPosition(GameObject theObject, Vector3 targetPosition, float duration)
     {
-        float time = 0;
+        theObject.transform.GetChild(1).GetComponent<Animator>().Play("attack");
+        yield return null;
+        /*float time = 0;
         Vector2 startPosition = theObject.transform.position;
         while (time < duration)
         {
@@ -3644,7 +3646,7 @@ public class MGameController : NetworkBehaviour
             yield return null;
         }
 
-        theObject.transform.position = startPosition;
+        theObject.transform.position = startPosition;*/
     }
     
       
